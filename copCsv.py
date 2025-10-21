665a1f23eb27789b01c9bf46463aec00dec7d006
Elhussin <hasin3112@gmail.com>
2025-10-22 01:24:48 +0300

    updat confilect

--- copCsv.py
+++ copCsv.py
@@ -3,45 +3,6 @@
 import csv
 import sqlite3
 
-# def copy_csv_to_db(csv_file, db_file):
-#     conn = sqlite3.connect(db_file)
-#     c = conn.cursor()
-#     c.execute("DROP TABLE IF EXISTS products")
-#     # c.execute()
-#     c.execute("""CREATE TABLE IF NOT EXISTS products(id INTEGER PRIMARY KEY AUTOINCREMENT,name TEXT,
-#     code TEXT,
-#     description TEXT,
-#     cost REAL,
-#     retail REAL,
-#     required_qty INTEGER,
-#     good_qty INTEGER,
-#     damaged_qty INTEGER,
-#     total_qty INTEGER
-# )""")
-
-        
-        
-#         # "CREATE TABLE IF NOT EXISTS products (id INTEGER PRIMARY KEY AUTOINCREMENT,name TEXT, code TEXT, description TEXT, cost REAL, retail REAL, required_qty INTEGER, good_qty INTEGER, damaged_qty INTEGER, total_qty INTEGER)")
-    
-#     with open(csv_file, 'r', encoding='utf-8') as f:
-#         reader = csv.reader(f)
-#         next(reader)  # Skip header row
-        
-#         for row in reader:
-#             c.execute("INSERT INTO products (name, code,description,cost, retail , required_qty, good_qty, damaged_qty, total_qty) VALUES (?, ?, ?, ?, ?, ?,?,?,?)", row)
-    
-#     conn.commit()
-#     conn.close()
-
-# if __name__ == "__main__":
-#     csv_file = "hussam2025.csv"  # Replace with your CSV file path
-#     db_file = "inventory.db"  # Replace with your database file path
-    
-#     copy_csv_to_db(csv_file, db_file)
-#     print("CSV data copied to database successfully.")
-import csv
-import sqlite3
-
 def copy_csv_to_db(csv_file, db_file):
     conn = sqlite3.connect(db_file)
     c = conn.cursor()
@@ -86,7 +47,7 @@ def copy_csv_to_db(csv_file, db_file):
 
 
 if __name__ == "__main__":
-    csv_file = "2025.csv"  # Replace with your CSV file path
+    csv_file = "all.csv"  # Replace with your CSV file path
     db_file = "inventory.db"  # Replace with your database file path
     
     copy_csv_to_db(csv_file, db_file)
